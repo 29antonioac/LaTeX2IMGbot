b1d18f394b671e0826c55be70d7f9f2ea3d5ed28
analca3 <analca3@gmail.com>
2015-07-22 12:39:12 +0200

    Added logging

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import telebot
 import time
+import sys
 from LaTeX2IMG import LaTeX2IMG
 from time import sleep
 from threading import current_thread
+from telebot import logging
 
 TOKEN = ''
 
@@ -34,6 +36,15 @@ def listener(messages):
 
 with open("token.txt","r") as file:
     TOKEN = file.readline().strip()
+
+logger = telebot.logger
+formatter = logging.Formatter('[%(asctime)s] %(thread)d {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+                                  '%m-%d %H:%M:%S')
+ch = logging.StreamHandler(sys.stdout)
+logger.addHandler(ch)
+logger.setLevel(logging.INFO)  # or use logging.INFO
+ch.setFormatter(formatter)
+
 tb = telebot.TeleBot(TOKEN)
 tb.set_update_listener(listener) #register listener
 tb.polling(True)
